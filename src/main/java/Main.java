b02c6aa93d746564b4d613cf2bfe921002397209
Alisina Barghandan <barghandanalisina@gmail.com>
2023-05-16 21:53:08 +0530

    small edits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import model.Login;
+import view.LoginView;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Login loginModel = new Login();
+        try {
+            // Use the platform look and feel for better integration
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                LoginView loginView = new LoginView(loginModel);
+                loginView.setVisible(true);
+            }
+        });
     }
 }
 
