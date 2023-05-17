e2f85689bab6fa6e55c772443726e73940442bc3
Ifhaam Zuhree <mzmifhaam@gmail.com>
2023-05-17 18:35:36 +0530

    Complted Placing the order fumction

--- src/main/java/view/PlaceOrderView.java
+++ src/main/java/view/PlaceOrderView.java
@@ -8,16 +8,17 @@ import java.awt.event.ActionEvent;
 
 import model.Order;
 
-public class MenuView extends JFrame
+public class PlaceOrderView extends JFrame
 {
     private String[] categories = {"Breakfast", "Lunch", "Fast Food"};
     private JComboBox box;
     private JTextField choice;
-    private JButton placeOrderButton;
+    private JButton completeButton;
+    private JButton generateMealButton;
     private Order orderModel;
 
-    public MenuView() {
-        super("Menu");
+    public PlaceOrderView() {
+        super("Place Order");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 300);
         setLocationRelativeTo(null);
@@ -32,7 +33,7 @@ public class MenuView extends JFrame
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
 
-        JLabel titleLabel = new JLabel("Menu");
+        JLabel titleLabel = new JLabel("Place Your Order");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
 
         JLabel categoryLabel = new JLabel("Categories:");
@@ -47,30 +48,27 @@ public class MenuView extends JFrame
                 // Add place order logic here
                 String category = (String) box.getSelectedItem();
                 choice.setText(category);
-                String menu = orderModel.displayMealMenu(category);
-                if (category.equals("Breakfast"))
-                {
-                    JOptionPane.showMessageDialog(MenuView.this, "Breakfast Menu:\n" + menu);
-                }
-                else if (category.equals("Lunch"))
-                {
-                    JOptionPane.showMessageDialog(MenuView.this, "Lunch Menu:\n" + menu);
-                }
-                else
-                {
-                    JOptionPane.showMessageDialog(MenuView.this, "Fast Food Menu:\n" + menu);
-                }
-                    
             }
         });
 
-        placeOrderButton = new JButton("Place Order");
-        placeOrderButton.addActionListener(new ActionListener() {
+        generateMealButton = new JButton("Generate Meal");
+        generateMealButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Add place order logic here
-                PlaceOrderView placeOrderView = new PlaceOrderView();
-                placeOrderView.setVisible(true);
+                String category = (String) box.getSelectedItem();
+                String meal = orderModel.autoGenerateMeal(category);
+                JOptionPane.showMessageDialog(PlaceOrderView.this, "Generated Meal:\n" + meal);
+            }
+        });
+
+        completeButton = new JButton("Complete Order");
+        completeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SummaryView summaryView = new SummaryView(orderModel);
+                summaryView.setVisible(true);
+                
             }
         });
 
@@ -105,7 +103,12 @@ public class MenuView extends JFrame
         gbc.gridy = 3;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
-        mainPanel.add(placeOrderButton, gbc);
+        mainPanel.add(generateMealButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.anchor = GridBagConstraints.CENTER;
+        mainPanel.add(completeButton, gbc);
 
         add(mainPanel);
     }
